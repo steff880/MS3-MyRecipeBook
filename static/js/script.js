13bf9ebe41f37cc67448a1b2654309399058dc9a
steff880 <stefan.d.yordanov@gmail.com>
2021-07-11 14:38:29 +0000

    Move global variables in checkIfPasswordsMatch function

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,10 @@
 // jQuery
 
-$(document).ready(function(){
-    $('.sidenav').sidenav({ edge: "right" });
+$(document).ready(function () {
+  $('.sidenav').sidenav({
+    edge: "right"
   });
+});
 
 
 
@@ -13,28 +15,28 @@ $(document).ready(function(){
 
 
 
+
+
+const checkIfPasswordsMatch = () => {
   let password = document.getElementById('password');
   let confirmPassword = document.getElementById('confirm_password');
 
-  const checkIfPasswordsMatch = () => {
-   
-    let messageText = document.querySelector('.message-text');
-    let regButton = document.querySelector('.btn-register');
+  let messageText = document.querySelector('.message-text');
+  let regButton = document.querySelector('.btn-register');
 
-    // Check if passwords match
+  // Check if passwords match
 
-    if (password.value !== confirmPassword.value) {
-      messageText.innerHTML = 'Passwords do not match!';
-      messageText.style.color = 'red';
+  if (password.value !== confirmPassword.value) {
+    messageText.innerHTML = 'Passwords do not match!';
+    messageText.style.color = 'red';
 
-      regButton.setAttribute('disabled', true);
-      regButton.classList.add('btn-disabled');
-    } else if( password.value === confirmPassword.value) {
-      messageText.innerHTML = 'Passwords match!';
-      messageText.style.color = 'green';
-      regButton.removeAttribute('disabled', true);
-      regButton.classList.remove('btn-disabled');
-    }
+    regButton.setAttribute('disabled', true);
+    regButton.classList.add('btn-disabled');
+  } else if (password.value === confirmPassword.value) {
+    messageText.innerHTML = 'Passwords match!';
+    messageText.style.color = 'green';
+    
+    regButton.removeAttribute('disabled', true);
+    regButton.classList.remove('btn-disabled');
   }
-
-
+}
