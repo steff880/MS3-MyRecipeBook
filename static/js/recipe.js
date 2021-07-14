468f027c44f84cf9edb3be0b5d3d7f7afcb17ba2
steff880 <stefan.d.yordanov@gmail.com>
2021-07-14 19:16:18 +0000

    Add delete ingredient functionality

--- static/js/recipe.js
+++ static/js/recipe.js
@@ -1,6 +1,7 @@
+'use strict';
 // Declare global variables
 
-// Sett max ingredients
+// Set max ingredients
 let maxIngredients = 30;
 let ingredientRow = document.getElementById('ingredient-row');
 let addIngredient = document.getElementById('add-ingredient');
@@ -11,18 +12,39 @@ let ingredient = 1;
 const appendIngredient = (e) => {
     e.preventDefault();
 
-    if(ingredient < maxIngredients) {
+    if (ingredient < maxIngredients) {
+        ingredient++;
         let newIngredientField = document.createElement('div');
-        newIngredientField.classList.add('input-field','col', 's12');
+        newIngredientField.classList.add('input-field', 'col', 's12');
         newIngredientField.innerHTML = `
             <i class="fas fa-list-ul prefix form-icon"></i>
             <input type="text" id="ingredients" name="ingredients" minlength="3" class="validate" required>
             <span class="helper-text grey-text darken-3"> Enter new ingredient</span>
             <a id="delete-btn" href="#" class="remove-field text-shadow">Delete <i class="fas fa-trash-alt"></i></a>`;
-        console.log(newIngredientField)
+
         ingredientRow.append(newIngredientField);
-        ingredient++;
+
     }
 }
 
-addIngredient.addEventListener('click', appendIngredient)
+// Delete New Ingredient
+
+const deleteIngredient = (event) => {
+    event.preventDefault();
+    // Get clicked element
+    let element = event.target;
+    // check if clicked element has the class we need
+    if (element.classList.contains('remove-field')) {
+        // remove last ingredient
+        if (ingredientRow.children.length > 1) {
+            ingredientRow.lastChild.remove();
+            ingredient--;
+        }
+    }
+}
+
+// Event Listeners
+
+document.addEventListener('click', deleteIngredient);
+
+addIngredient.addEventListener('click', appendIngredient);
