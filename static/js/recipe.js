c8059499466a0031b77cf488bc54c9bbbe7697bb
steff880 <stefan.d.yordanov@gmail.com>
2021-07-14 16:05:33 +0000

    Add, Add New Ingredient functionality

---
+++ static/js/recipe.js
@@ -0,0 +1,28 @@
+// Declare global variables
+
+// Sett max ingredients
+let maxIngredients = 30;
+let ingredientRow = document.getElementById('ingredient-row');
+let addIngredient = document.getElementById('add-ingredient');
+let ingredient = 1;
+
+// Append new ingredient
+
+const appendIngredient = (e) => {
+    e.preventDefault();
+
+    if(ingredient < maxIngredients) {
+        let newIngredientField = document.createElement('div');
+        newIngredientField.classList.add('input-field','col', 's12');
+        newIngredientField.innerHTML = `
+            <i class="fas fa-list-ul prefix form-icon"></i>
+            <input type="text" id="ingredients" name="ingredients" minlength="3" class="validate" required>
+            <span class="helper-text grey-text darken-3"> Enter new ingredient</span>
+            <a id="delete-btn" href="#" class="remove-field text-shadow">Delete <i class="fas fa-trash-alt"></i></a>`;
+        console.log(newIngredientField)
+        ingredientRow.append(newIngredientField);
+        ingredient++;
+    }
+}
+
+addIngredient.addEventListener('click', appendIngredient)
