ee041d059c26ad8b80c36adcbbd72f265443064d
steff880 <stefan.d.yordanov@gmail.com>
2021-07-16 15:01:41 +0000

    Check if user is not in session and before attempting a login

--- app.py
+++ app.py
@@ -91,32 +91,38 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if "user" not in session:
+        # only if there isn't current session["user"]
+        if request.method == "POST":
+            # check if username exists in db
+            existing_user = mongo.db.users.find_one(
+                {"username": request.form.get("username").lower()})
 
-    if request.method == "POST":
-        # check if username exists in db
-        existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
+            if existing_user:
+                # ensure hashed password matches user input
+                if check_password_hash(
+                    existing_user['password'], request.form.get(
+                        "password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome back {} ! Let's start creating!".format(
+                            request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
+                else:
+                    # invalid password match
+                    flash(
+                        "Incorrect Username and/or Password, please try again")
+                    return redirect(url_for("login"))
 
-        if existing_user:
-            # ensure hashed password matches user input
-            if check_password_hash(
-                existing_user['password'], request.form.get(
-                    "password")):
-                session["user"] = request.form.get("username").lower()
-                flash("Welcome back {} ! Let's start creating!".format(
-                        request.form.get("username")))
-                return redirect(url_for("profile", username=session["user"]))
             else:
-                # invalid password match
+                # username doesn't exist
                 flash("Incorrect Username and/or Password, please try again")
                 return redirect(url_for("login"))
 
-        else:
-            # username doesn't exist
-            flash("Incorrect Username and/or Password, please try again")
-            return redirect(url_for("login"))
+        return render_template("login.html")
 
-    return render_template("login.html")
+    # user is already logged-in, direct them to their profile
+    return redirect(url_for("profile", username=session["user"]))
 
 
 # --------------------- Profile
