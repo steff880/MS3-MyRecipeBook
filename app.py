8e31cc0ea59a1e4a8dbb51fc8ccdc713c67ebf3b
steff880 <stefan.d.yordanov@gmail.com>
2021-07-22 15:04:07 +0000

    Add 500 error page and move error pages to error_pages dir

--- app.py
+++ app.py
@@ -345,12 +345,12 @@ def subscribe():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"), 404
+    return render_template("error_pages/404.html"), 404
 
 
 @app.errorhandler(500)
 def server_error(e):
-    return render_template("500.html"), 500
+    return render_template("error_pages/500.html"), 500
 
 
 # ---------- Run app
